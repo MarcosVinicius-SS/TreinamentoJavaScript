d03d7b40b0eba0fdffdaef88bc13b43f9bceb4ce
MarcosVinicius-SS <MARCOSVINICIUSDEV0@GMAIL.COM>
2026-05-26 16:23:09 -0300

    alteraçoes no css, html, css
    feat: efeito de livro fechado abrindo

--- livro de dragoes js/script.js
+++ livro de dragoes js/script.js
@@ -1,41 +1,30 @@
-const dragoes = [
-    {
-        classe: "Fúria da Noite",
-        perigo: "Alto",
-        descrição: "Esse dragão é a cria diabolica do Raio e da propia morte",
-        conselho: "escondassse e reze para que ele não te encontre"
-    },
-    {
-        classe: "Gronckle",
-        perigo: "Médio",
-        descrição: "Esse dragão é um dos mais comuns, ele é pequeno e tem uma carapaça dura",
-        conselho: "Ele é um dragão amigável, mas cuidado com suas caudas"
-    },
-    {
-        classe: "Nadder Mortal",
-        perigo: "Alto",
-        descrição: "Dragão agil e inteligente, sua cauda tem espinhos que ele lança nos inimigos",
-        conselho: "Ele é um dragão agressivo e desconfiado, então mantenha distância e não o provoque"
-        
-    },
+const coverRight = document.querySelector('.cover.cover-right')
+const coverLeft = document.querySelector('.cover.cover-left')
 
-]
-    const catalogo = document.getElementById("catalogo");
-    dragoes.forEach(function(dragon) {
-        const card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `
-        <h2>${dragon.classe}</h2>
-        <p class="perigo">perigo: ${dragon.perigo}</p>
-        <p class="descricao">${dragon.descrição}</p>
-        <p class="conmselho">${dragon.conselho}</p>
-        `;
-        catalogo.appendChild(card);
-    });
+// abre o livro automaticamente
+const wrapper = document.querySelector('.wrapper')
 
-    const campoBusca = document.getElementById("busca");
-    campoBusca.addEventListener("input", function() {
-        const termoBusca = campoBusca.valor.toLowerCase();
-        const cards = document.querySelectorAll(".card");
-        cards.forEach(function(card) {
-            const texto = card.innerText
+setTimeout(() => {
+    coverRight.classList.add('turn')
+    wrapper.classList.add('aberto')
+}, 2000)
+
+setTimeout(() => {
+    coverRight.style.display = 'none'
+    coverLeft.style.display = 'none'
+}, 3200)
+// botoes de virar a pagina
+const pageTurnBtns = document.querySelectorAll('.nextprev-btn')
+
+pageTurnBtns.forEach(function(btn) {
+    btn.onclick = function() {
+        const pageId = btn.getAttribute('data-page')
+        const page = document.getElementById(pageId)
+
+        if (page.classList.contains('turn')) {
+            page.classList.remove('turn')
+        } else {
+            page.classList.add('turn')
+        }
+    }
+})
